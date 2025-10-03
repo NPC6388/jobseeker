bf2082d49a4196df9810c6d6ffd9bb735f7d7705
NPC6388 <whoisjohngalt9@pm.me>
2025-10-03 16:20:18 -0700

    Fix: Correct PDF parse library usage (pdfParse.pdf)

--- src/webServer.js
+++ src/webServer.js
@@ -5,7 +5,7 @@ const socketIo = require('socket.io');
 const fs = require('fs-extra');
 const path = require('path');
 const multer = require('multer');
-const pdf = require('pdf-parse');
+const pdfParse = require('pdf-parse');
 const JobSeeker = require('./jobSeeker');
 const ResumeTailor = require('./resumeTailor');
 const CoverLetterGenerator = require('./coverLetterGenerator');
@@ -245,7 +245,7 @@ app.get('/api/resume-info', async (req, res) => {
                 if (ext === '.pdf') {
                     // Parse PDF
                     const dataBuffer = await fs.readFile(fullPath);
-                    const data = await pdf(dataBuffer);
+                    const data = await pdfParse.pdf(dataBuffer);
                     resumeText = data.text;
                 } else if (ext === '.docx' || ext === '.doc') {
                     // Parse DOCX using mammoth
